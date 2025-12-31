56a27ec1301071c54cb4c429cad543670ba6ca9a
Monther5 <159758834+Monther5@users.noreply.github.com>
2025-12-31 18:20:25 +0200

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -16,28 +16,7 @@ use Illuminate\Validation\ValidationException;
  */
 class AuthController extends BaseController
 {
-    /**
-     * @OA\Post(
-     *      path="/api/register",
-     *      operationId="registerUser",
-     *      tags={"Authentication"},
-     *      summary="Register new user",
-     *      description="Registers a new user and returns token",
-     *      @OA\RequestBody(
-     *          required=true,
-     *          @OA\JsonContent(
-     *              required={"name","email","password"},
-     *              @OA\Property(property="name", type="string"),
-     *              @OA\Property(property="email", type="string", format="email"),
-     *              @OA\Property(property="password", type="string", format="password", minLength=8),
-     *          ),
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Successful operation",
-     *       ),
-     * )
-     */
+
     public function register(Request $request)
     {
         $validated = $request->validate([
@@ -60,27 +39,7 @@ class AuthController extends BaseController
         ]);
     }
 
-    /**
-     * @OA\Post(
-     *      path="/api/login",
-     *      operationId="loginUser",
-     *      tags={"Authentication"},
-     *      summary="Login user",
-     *      description="Logs in user and returns token",
-     *      @OA\RequestBody(
-     *          required=true,
-     *          @OA\JsonContent(
-     *              required={"email","password"},
-     *              @OA\Property(property="email", type="string", format="email"),
-     *              @OA\Property(property="password", type="string", format="password"),
-     *          ),
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Successful operation",
-     *       ),
-     * )
-     */
+
     public function login(Request $request)
     {
         if (!auth()->attempt($request->only('email', 'password'))) {
